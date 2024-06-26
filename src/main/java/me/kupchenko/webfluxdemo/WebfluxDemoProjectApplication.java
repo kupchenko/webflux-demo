5b40f990815cdbabc99d22fa102874b98d7df843
Dmitrii Kupchenko <kupchenko.work@gmail.com>
2024-06-26 19:23:43 +0400

    Init

---
+++ src/main/java/me/kupchenko/webfluxdemo/WebfluxDemoProjectApplication.java
@@ -0,0 +1,13 @@
+package me.kupchenko.webfluxdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxDemoProjectApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(WebfluxDemoProjectApplication.class, args);
+  }
+
+}
---
+++ src/test/java/me/kupchenko/webfluxdemo/WebfluxDemoProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package me.kupchenko.webfluxdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxDemoProjectApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
